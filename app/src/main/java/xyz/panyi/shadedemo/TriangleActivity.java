e3553826d314b682eee4072b059723db65c38401
yixin.panyi <panyi@yixin.im>
2020-09-30 17:35:42 +0800

    update ppt

---
+++ app/src/main/java/xyz/panyi/shadedemo/TriangleActivity.java
@@ -0,0 +1,12 @@
+package xyz.panyi.shadedemo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TriangleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
